c15ac0aeafa4df22c83311d73355c5c94bcc9f5e
Markus Stöbe <markus@stoebe.me>
2017-07-27 18:05:38 +0200

    Initial Commit

---
+++ VisionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionDemo
+//
+//  Created by Markus Stöbe on 27.07.17.
+//  Copyright © 2017 Markus Stöbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
