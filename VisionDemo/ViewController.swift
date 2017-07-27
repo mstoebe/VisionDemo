50dc739026b176c86eb6f32e9c9063ef161eab11
Markus Stöbe <markus@stoebe.me>
2017-07-27 18:24:22 +0200

    prepared live video feed

--- VisionDemo/ViewController.swift
+++ VisionDemo/ViewController.swift
@@ -7,18 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var livePreView: UIView!
+	@IBOutlet weak var selectionView: UIView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		setupVideoPreview(withView: livePreView)
 	}
+
+	//******************************************************************************************************************
+	//* MARK: - Videofeed von der Kamera
+	//******************************************************************************************************************
+	public func setupVideoPreview (withView : UIView) {
+		//AVCaptureSession einrichten und Kamera als Quelle wählen
+		let liveSession = AVCaptureSession()
+		liveSession.sessionPreset = AVCaptureSession.Preset.photo
+		if let cam   = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+			let input = try? AVCaptureDeviceInput(device: cam) {
+			liveSession.addInput(input)
+		}
+
+		//Preview-Layer erzeugen und an die View im UI hängen
+		let cameraPreviewLayer = AVCaptureVideoPreviewLayer(session:liveSession)
+		cameraPreviewLayer.frame = withView.bounds
+		cameraPreviewLayer.videoGravity = .resize
+		cameraPreviewLayer.connection?.videoOrientation = .landscapeRight
+		withView.layer.addSublayer(cameraPreviewLayer)
+
+		//Video-Stream starten
+		liveSession.startRunning()
+	}
+
+
+
 
 
 }
