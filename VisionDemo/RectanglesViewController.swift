e9bfef0e6fa7966395a828cc2636a348dc08ad6d
Markus Stöbe <stoebe@oecon-line.de>
2017-08-07 11:37:11 +0200

    added rectangle recognition

--- VisionDemo/RectanglesViewController.swift
+++ VisionDemo/RectanglesViewController.swift
@@ -7,29 +7,67 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
 class RectanglesViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	@IBOutlet weak var imageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let image = UIImage(named: "rects")
+		let cgimage = image?.cgImage
+		
+		guard image != nil, cgimage != nil else {
+			print ("error while loading and converting image")
+			return
+		}
+		
+		imageView.image = image
+		
+		//Requesthandler anlegen
+		let requestHandler = VNImageRequestHandler(cgImage: cgimage!)
+		
+		//VNRequest erzeugen
+		let request = VNDetectRectanglesRequest { (request, error) in
+			//completion-handler
+			print ("finished detection")
+			
+			if let results = request.results as? [VNRectangleObservation] {
+				print ("found \(results.count) results")
+				
+				for result in results {
+					
+					//Größe des Ergebnis umwandeln
+					let boundingBox     = result.boundingBox
+					let scaledImageRect = AVMakeRect(aspectRatio: image!.size,
+					                                 insideRect: self.view.frame)
+					self.imageView.frame = scaledImageRect
+					
+					let size = CGSize(width:  boundingBox.width  * scaledImageRect.size.width,
+					                  height: boundingBox.height * scaledImageRect.size.height)
+					let origin = CGPoint(x: scaledImageRect.origin.x + (boundingBox.origin.x * scaledImageRect.size.width),
+					                     y: scaledImageRect.origin.y + ((1 - boundingBox.origin.y) * scaledImageRect.size.height) - size.height)
+					
+					let layer = CAShapeLayer()
+					layer.frame = CGRect(origin: origin, size: size)
+					layer.borderColor = UIColor.red.cgColor
+					layer.borderWidth = 2
+					
+					self.imageView.layer.addSublayer(layer)
+				}
+			}
+		}
+		
+		request.maximumObservations = 0
+		request.minimumConfidence   = 0.5
+		
+		//perform request
+		do {
+			try requestHandler.perform([request])
+		} catch {
+			print(error)
+		}
+	}
 }
