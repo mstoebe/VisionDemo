41af47ce71fbb6188f4aac7b60c85758f41513ff
Markus Stöbe <stoebe@oecon-line.de>
2017-08-07 11:55:01 +0200

    simplified landmarks request, changed seque

--- VisionDemo/FaceViewController.swift
+++ VisionDemo/FaceViewController.swift
@@ -5,31 +5,64 @@
 //  Created by Markus Stöbe on 27.07.17.
 //  Copyright © 2017 Markus Stöbe. All rights reserved.
 //
-
 import UIKit
+import Vision
+import AVFoundation
 
 class FaceViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	@IBOutlet weak var imageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let image = UIImage(named: "faces")
+		imageView.image = image
+		let cgimage = image?.cgImage
+		
+		guard image != nil, cgimage != nil else {
+			print ("error while loading and converting image")
+			return
+		}
+		
+		//Requesthandler anlegen
+		let requestHandler = VNImageRequestHandler(cgImage: cgimage!)
+		
+		//VNRequest erzeugen
+		let request = VNDetectFaceRectanglesRequest { (request, error) in
+			//completion-handler
+			print ("finished detection")
+			
+			if let results = request.results as? [VNFaceObservation] {
+				print ("found \(results.count) results")
+				
+				for result in results {
+					
+					//Größe des Ergebnis umwandeln
+					let boundingBox     = result.boundingBox
+					let scaledImageRect = AVMakeRect(aspectRatio: image!.size, insideRect: self.view.frame)
+					self.imageView.frame = scaledImageRect
+					
+					let size = CGSize(width:  boundingBox.width  * scaledImageRect.size.width,
+					                  height: boundingBox.height * scaledImageRect.size.height)
+					let origin = CGPoint(x: scaledImageRect.origin.x + (boundingBox.origin.x * scaledImageRect.size.width),
+					                     y: scaledImageRect.origin.y + ((1 - boundingBox.origin.y) * scaledImageRect.size.height) - size.height)
+					
+					//Ergebnis einrahmen
+					let layer = CAShapeLayer()
+					layer.frame = CGRect(origin: origin, size: size)
+					layer.borderColor = UIColor.red.cgColor
+					layer.borderWidth = 2
+					self.imageView.layer.addSublayer(layer)
+				}
+			}
+		}
+		
+		//perform request
+		do {
+			try requestHandler.perform([request])
+		} catch {
+			print(error)
+		}
+	}
 
 }
--- VisionDemo/LandmarksViewController.swift
+++ VisionDemo/LandmarksViewController.swift
@@ -11,131 +11,87 @@ import Vision
 import AVFoundation
 
 class LandmarksViewController: UIViewController {
-
 	@IBOutlet weak var imageView: UIImageView!
-
-	let image = UIImage(named: "faces.jpg")
+	
+	let image = UIImage(named: "faces")
 	var requestHandler : VNImageRequestHandler?
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		imageView.image = image
+		findLandmarks(in: image)
 	}
-
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		findFaces(in: image)
-	}
-
-	func findFaces(in image:UIImage?) {
+	
+	func findLandmarks(in image:UIImage?) {
+		//Bild laden
 		let cgimage = image?.cgImage
 		guard image != nil, cgimage != nil else {
 			print ("error while loading and converting image")
 			return
 		}
-
+		
 		//Requesthandler anlegen
-		self.requestHandler = VNImageRequestHandler(cgImage: cgimage! , options: [:])
+		self.requestHandler = VNImageRequestHandler(cgImage: cgimage!)
 		guard self.requestHandler != nil else {
 			print ("error generating requestHandler")
 			return
 		}
-
-		//VNRequest erzeugen
-		let request = VNDetectFaceRectanglesRequest { (request, error) in
-			//completion-handler
-			print ("finished face detection")
-
-			if let error = error {
-				print ("…with error:" + error.localizedDescription)
-				return
-			}
-
-			if let results = request.results as? [VNFaceObservation] {
-				print ("found \(results.count) faces")
-
-				for result in results {
-					//Größe des Ergebnis umwandeln
-					let boundingBox     = result.boundingBox
-					let scaledImageRect = AVMakeRect(aspectRatio: self.image!.size, insideRect: self.view.frame)
-					self.imageView.frame = scaledImageRect
-
-					let size = CGSize(width:  boundingBox.width  * scaledImageRect.size.width,
-					                  height: boundingBox.height * scaledImageRect.size.height)
-					let origin = CGPoint(x: scaledImageRect.origin.x + (boundingBox.origin.x * scaledImageRect.size.width),
-					                     y: scaledImageRect.origin.y + ((1 - boundingBox.origin.y) * scaledImageRect.size.height) - size.height)
-
-					//Ergebnis einrahmen
-					let layer = CAShapeLayer()
-					layer.frame = CGRect(origin: origin, size: size)
-					layer.borderColor = UIColor.red.cgColor
-					layer.borderWidth = 2
-					self.imageView.layer.addSublayer(layer)
-				}
-
-				//jetzt gehts weiter mit den Face Landmarks
-				self.findLandmarks(in: results)
-			}
-		}
-
-		//perform request
-		do {
-			try requestHandler!.perform([request])
-		} catch {
-			print(error)
-		}
-	}
-
-	func findLandmarks(in faces:[VNFaceObservation]) {
+		
 		print("...now looking for noses...")
 		var allNoses = [[CGPoint]]()
-
+		
 		//VNRequest erzeugen
 		let request = VNDetectFaceLandmarksRequest { (request, error) in
 			//completion-handler
 			print ("finished searching for noses")
-
+			
 			//wenns einen Fehler gab, aufhören
 			if let error = error {
 				print ("…with error:" + error.localizedDescription)
 				return
 			}
-
+			
 			if let results = request.results as? [VNFaceObservation] {
 				print ("found \(results.count) landmark-sets")
-
+				
 				for face in results {
 					if let nose = face.landmarks?.nose {
-						var pointsForThisNose = [CGPoint]()
-
 						//calc bounding box for face, again
 						let boundingBox     = face.boundingBox
 						let scaledImageRect = AVMakeRect(aspectRatio: self.image!.size, insideRect: self.view.frame)
 						self.imageView.frame = scaledImageRect
-
+						
 						let size = CGSize(width:  boundingBox.width  * scaledImageRect.size.width,
 						                  height: boundingBox.height * scaledImageRect.size.height)
 						let origin = CGPoint(x: scaledImageRect.origin.x + (boundingBox.origin.x * scaledImageRect.size.width),
 						                     y: scaledImageRect.origin.y + ((1 - boundingBox.origin.y) * scaledImageRect.size.height) - size.height)
-
+						
+						//Ergebnis einrahmen
+						let layer = CAShapeLayer()
+						layer.frame = CGRect(origin: origin, size: size)
+						layer.borderColor = UIColor.red.cgColor
+						layer.borderWidth = 2
+						self.imageView.layer.addSublayer(layer)
+						
+						//Nasenpunkte konvertieren
+						var pointsForThisNose = [CGPoint]()
+						
 						for i in 0...nose.pointCount {
 							let point    = nose.point(at: i)
 							let newPoint = CGPoint(x: origin.x + (     CGFloat(point.x)  * size.width),
 							                       y: origin.y + ((1 - CGFloat(point.y)) * size.height))
 							pointsForThisNose.append(newPoint)
 						}
-
+						
 						allNoses.append(pointsForThisNose)
 					}
 				}
-
+				
+				//Nasen nachziehen
 				self.highlightNoses(in: self.imageView, noses: allNoses)
 			}
 		}
-
-		//die bereits gefundenen Gesichter übergeben
-		request.inputFaceObservations = faces
-
+		
 		//…und Nasen suchen lassen!
 		do {
 			try self.requestHandler!.perform([request])
@@ -143,13 +99,13 @@ class LandmarksViewController: UIViewController {
 			print(error)
 		}
 	}
-
+	
 	func highlightNoses(in image:UIImageView, noses:[[CGPoint]]) {
 		for nose in noses {
 			let layer = CAShapeLayer()
 			layer.strokeColor = UIColor.green.cgColor
 			layer.lineWidth   = 1.0
-
+			
 			let path = UIBezierPath()
 			for point in nose {
 				if nose.index(of: point) == 0 {
