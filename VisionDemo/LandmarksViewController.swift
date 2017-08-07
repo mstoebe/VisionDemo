594db6fd8eb3192083ebc01822443afcfb3b554d
Markus Stöbe <markus@stoebe.me>
2017-08-07 11:30:57 +0200

    added images, tracking and landmarks

--- VisionDemo/LandmarksViewController.swift
+++ VisionDemo/LandmarksViewController.swift
@@ -7,29 +7,162 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
 class LandmarksViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet weak var imageView: UIImageView!
 
-        // Do any additional setup after loading the view.
-    }
+	let image = UIImage(named: "faces.jpg")
+	var requestHandler : VNImageRequestHandler?
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		imageView.image = image
+	}
 
-    /*
-    // MARK: - Navigation
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		findFaces(in: image)
+	}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	func findFaces(in image:UIImage?) {
+		let cgimage = image?.cgImage
+		guard image != nil, cgimage != nil else {
+			print ("error while loading and converting image")
+			return
+		}
 
+		//Requesthandler anlegen
+		self.requestHandler = VNImageRequestHandler(cgImage: cgimage! , options: [:])
+		guard self.requestHandler != nil else {
+			print ("error generating requestHandler")
+			return
+		}
+
+		//VNRequest erzeugen
+		let request = VNDetectFaceRectanglesRequest { (request, error) in
+			//completion-handler
+			print ("finished face detection")
+
+			if let error = error {
+				print ("…with error:" + error.localizedDescription)
+				return
+			}
+
+			if let results = request.results as? [VNFaceObservation] {
+				print ("found \(results.count) faces")
+
+				for result in results {
+					//Größe des Ergebnis umwandeln
+					let boundingBox     = result.boundingBox
+					let scaledImageRect = AVMakeRect(aspectRatio: self.image!.size, insideRect: self.view.frame)
+					self.imageView.frame = scaledImageRect
+
+					let size = CGSize(width:  boundingBox.width  * scaledImageRect.size.width,
+					                  height: boundingBox.height * scaledImageRect.size.height)
+					let origin = CGPoint(x: scaledImageRect.origin.x + (boundingBox.origin.x * scaledImageRect.size.width),
+					                     y: scaledImageRect.origin.y + ((1 - boundingBox.origin.y) * scaledImageRect.size.height) - size.height)
+
+					//Ergebnis einrahmen
+					let layer = CAShapeLayer()
+					layer.frame = CGRect(origin: origin, size: size)
+					layer.borderColor = UIColor.red.cgColor
+					layer.borderWidth = 2
+					self.imageView.layer.addSublayer(layer)
+				}
+
+				//jetzt gehts weiter mit den Face Landmarks
+				self.findLandmarks(in: results)
+			}
+		}
+
+		//perform request
+		do {
+			try requestHandler!.perform([request])
+		} catch {
+			print(error)
+		}
+	}
+
+	func findLandmarks(in faces:[VNFaceObservation]) {
+		print("...now looking for noses...")
+		var allNoses = [[CGPoint]]()
+
+		//VNRequest erzeugen
+		let request = VNDetectFaceLandmarksRequest { (request, error) in
+			//completion-handler
+			print ("finished searching for noses")
+
+			//wenns einen Fehler gab, aufhören
+			if let error = error {
+				print ("…with error:" + error.localizedDescription)
+				return
+			}
+
+			if let results = request.results as? [VNFaceObservation] {
+				print ("found \(results.count) landmark-sets")
+
+				for face in results {
+					if let nose = face.landmarks?.nose {
+						var pointsForThisNose = [CGPoint]()
+
+						//calc bounding box for face, again
+						let boundingBox     = face.boundingBox
+						let scaledImageRect = AVMakeRect(aspectRatio: self.image!.size, insideRect: self.view.frame)
+						self.imageView.frame = scaledImageRect
+
+						let size = CGSize(width:  boundingBox.width  * scaledImageRect.size.width,
+						                  height: boundingBox.height * scaledImageRect.size.height)
+						let origin = CGPoint(x: scaledImageRect.origin.x + (boundingBox.origin.x * scaledImageRect.size.width),
+						                     y: scaledImageRect.origin.y + ((1 - boundingBox.origin.y) * scaledImageRect.size.height) - size.height)
+
+						for i in 0...nose.pointCount {
+							let point    = nose.point(at: i)
+							let newPoint = CGPoint(x: origin.x + (     CGFloat(point.x)  * size.width),
+							                       y: origin.y + ((1 - CGFloat(point.y)) * size.height))
+							pointsForThisNose.append(newPoint)
+						}
+
+						allNoses.append(pointsForThisNose)
+					}
+				}
+
+				self.highlightNoses(in: self.imageView, noses: allNoses)
+			}
+		}
+
+		//die bereits gefundenen Gesichter übergeben
+		request.inputFaceObservations = faces
+
+		//…und Nasen suchen lassen!
+		do {
+			try self.requestHandler!.perform([request])
+		} catch {
+			print(error)
+		}
+	}
+
+	func highlightNoses(in image:UIImageView, noses:[[CGPoint]]) {
+		for nose in noses {
+			let layer = CAShapeLayer()
+			layer.strokeColor = UIColor.green.cgColor
+			layer.lineWidth   = 1.0
+
+			let path = UIBezierPath()
+			for point in nose {
+				if nose.index(of: point) == 0 {
+					path.move(to: point)
+				} else if nose.index(of: point) == nose.count-1 {
+					path.move(to: point)
+				} else {
+					path.addLine(to: point)
+					path.move(to: point)
+				}
+			}
+			layer.path = path.cgPath
+			image.layer.addSublayer(layer)
+		}
+	}
 }
--- VisionDemo/TrackingViewController.swift
+++ VisionDemo/TrackingViewController.swift
@@ -10,49 +10,138 @@ import UIKit
 import AVFoundation
 import Vision
 
-class TrackingViewController: UIViewController {
+class TrackingViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+	//******************************************************************************************************************
+	//* MARK: - Outlets
+	//******************************************************************************************************************
 	@IBOutlet weak var livePreView: UIView!
-	@IBOutlet weak var selectionView: UIView!
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
+	@IBOutlet weak var selectionView: UIView! {
+		didSet {
+			self.selectionView.backgroundColor = .clear
+			self.selectionView.layer.borderColor = UIColor.blue.cgColor
+			self.selectionView.layer.borderWidth = 2
+		}
 	}
 
+	//******************************************************************************************************************
+	//* MARK: - Lifecycle
+	//******************************************************************************************************************
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
+
+		//Video-Stream einrichten
 		setupVideoPreview(withView: livePreView)
+
+		//Auswahl zu Beginn ausblenden, Frame wird später durch den Tap-Delegate neu gesetzt
+		selectionView.frame = .zero
 	}
 
-	
-
-
-
-
-
 	//******************************************************************************************************************
-	//* MARK: - Videofeed von der Kamera
+	//* MARK: - Videofeed von der Kamera einbinden
 	//******************************************************************************************************************
+	private let liveSession = AVCaptureSession()
+	private var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
+
 	public func setupVideoPreview (withView : UIView) {
 		//AVCaptureSession einrichten und Kamera als Quelle wählen
-		let liveSession = AVCaptureSession()
-		liveSession.sessionPreset = AVCaptureSession.Preset.photo
-		if let cam   = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+		self.liveSession.sessionPreset = AVCaptureSession.Preset.photo
+		if let  cam   = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
 			let input = try? AVCaptureDeviceInput(device: cam) {
-			liveSession.addInput(input)
+			self.liveSession.addInput(input)
 		}
 
 		//Preview-Layer erzeugen und an die View im UI hängen
-		let cameraPreviewLayer = AVCaptureVideoPreviewLayer(session:liveSession)
-		cameraPreviewLayer.frame = withView.bounds
-		cameraPreviewLayer.videoGravity = .resize
-		cameraPreviewLayer.connection?.videoOrientation = .landscapeRight
-		withView.layer.addSublayer(cameraPreviewLayer)
+		self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session:liveSession)
+		self.cameraPreviewLayer!.frame = withView.bounds
+		self.cameraPreviewLayer!.videoGravity = .resize
+		self.cameraPreviewLayer!.connection?.videoOrientation = .landscapeRight
+		withView.layer.addSublayer(self.cameraPreviewLayer!)
 
 		//Video-Stream starten
-		liveSession.startRunning()
+		self.liveSession.startRunning()
+
+		//ViewController als OutpoutDelegate eintragen (dieser Teil löst das eigentliche Tracking-Verfahren aus)
+		let output = AVCaptureVideoDataOutput()
+		output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+		self.liveSession.addOutput(output)
 	}
 
+	//******************************************************************************************************************
+	//* MARK: - Objekt-Tracking
+	//******************************************************************************************************************
+	//* Delegate Callback (AVCaptureVideoDataOutputSampleBufferDelegate)
+	//******************************************************************************************************************
+	private let sequenceRequestHandler = VNSequenceRequestHandler()
+	private var lastObservation: VNDetectedObjectObservation?
+
+	func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+		guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
+			  let lastObservation = self.lastObservation
+		else {
+			return
+		}
+
+		// create the request
+		let request = VNTrackObjectRequest(detectedObjectObservation: lastObservation, completionHandler: self.objectTrackingDidFinish)
+		// set the accuracy to high
+		// this is slower, but it works a lot better
+		request.trackingLevel = .accurate
+
+		// perform the request
+		do {
+			try self.sequenceRequestHandler.perform([request], on: pixelBuffer)
+		} catch {
+			print("Throws: \(error)")
+		}
+	}
+
+	//******************************************************************************************************************
+	//* completionHandler für den VNTrackObjectRequest
+	//******************************************************************************************************************
+	private func objectTrackingDidFinish(_ request: VNRequest, error: Error?) {
+		// Dispatch to the main queue because we are touching non-atomic, non-thread safe properties of the view controller
+		DispatchQueue.main.async {
+			// make sure we have an actual result
+			guard let newObservation = request.results?.first as? VNDetectedObjectObservation else { return }
+
+			// prepare for next loop
+			self.lastObservation = newObservation
+
+			// check the confidence level before updating the UI
+			guard newObservation.confidence >= 0.3 else {
+				// hide the rectangle when we lose accuracy so the user knows something is wrong
+				self.selectionView.frame = .zero
+				return
+			}
+
+			// calculate view rect
+			var transformedRect = newObservation.boundingBox
+			transformedRect.origin.y = 1 - transformedRect.origin.y
+			let convertedRect = self.cameraPreviewLayer?.layerRectConverted(fromMetadataOutputRect: transformedRect)
+
+			// move the highlight view
+			self.selectionView.frame = convertedRect ?? .zero
+		}
+	}
+
+	//******************************************************************************************************************
+	//* MARK: - Tap-Handler
+	//******************************************************************************************************************
+	@IBAction private func userTapped(_ sender: UITapGestureRecognizer) {
+		//User-Tap markieren
+		self.selectionView.frame.size = CGSize(width: 120, height: 120)
+		self.selectionView.center = sender.location(in: self.view)
+
+		//Bildausschnitt zur Observation speichern/vormerken
+		var convertedRect = self.cameraPreviewLayer?.metadataOutputRectConverted(fromLayerRect: self.selectionView.frame)
+		if convertedRect != nil {
+			convertedRect!.origin.y = 1 - convertedRect!.origin.y
+			// set the observation
+			let newObservation = VNDetectedObjectObservation(boundingBox: convertedRect!)
+			self.lastObservation = newObservation
+		}
+	}
 
 
 
